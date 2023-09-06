b3a28df8874cafe78bdd97e77484712e16c8b62a
Leonardo Melati <leomelati@gmail.com>
2023-09-06 00:24:59 -0300

    Exericios 1 e 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,25 +3,35 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+function useSessionStorageState(key, defaultValue = '') {
   // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  // A ideia de criar uma função serve para melhorias de performance
+  const [value, setValue] = React.useState(
+    () => JSON.parse(window.sessionStorage.getItem(key)) ?? defaultValue,
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.sessionStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+function Greeting({initialName = ''}) {
+  const [value, setValue] = useSessionStorageState('name', initialName)
 
   function handleChange(event) {
-    setName(event.target.value)
+    setValue(event.target.value)
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={value} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {value ? <strong>Hello {value}</strong> : 'Please type your name'}
     </div>
   )
 }
